c2489894ba24d83a4cf3eb54e34ceb3d825c619e
Taylor Venissat <tvenissat@gmail.com>
2020-02-10 15:15:12 -0600

    Replace Unicode value with Unicode character

--- mprisindicator@tvenissat.github.io/widgets.js
+++ mprisindicator@tvenissat.github.io/widgets.js
@@ -1584,7 +1584,7 @@ var MprisIndicatorButton = GObject.registerClass({
                     this.menu.toggle();
             }
             if (visible) {
-                let delimiter = ' \u2022 '; // Unicode Character 'BULLET' (U+2022).
+                let delimiter = ' • '; // Unicode Character 'BULLET' (U+2022).
                 let delimiterLength = delimiter.length;
                 let suffix = '...';
                 let suffixLength = suffix.length;
